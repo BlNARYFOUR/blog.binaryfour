a1f5f5e27df261c406050ce999dde7f52372fd3d
Brend Lambert <lambertbrend@gmail.com>
2019-09-26 02:54:45 +0200

    Yaaayy login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {BlogService} from "./services/blog.service";
 
 @Component({
   selector: 'app-root',
+    providers: [BlogService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'blog-binaryfour';
+  loginActive: boolean = false;
+
+    constructor(
+        private _blogService: BlogService
+    ) { }
+
+    toggleLogin() {
+        this.loginActive = !this.loginActive;
+    }
 }
